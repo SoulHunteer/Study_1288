f8e9cebcb42023045716f2e2b04f810cab398b39
SoulHunteer <seregalutovinov@yandex.ru>
2022-12-21 18:20:51 +0300

    Our first version

---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+1. Есть список всех пользователей сайта. Изначально он пустой
+users = {}
+Необходимо запрашивать информацию и добавлять нового пользователя в словарь
+password =
+username =
+id = username_<порядковый номер>
+После этого, нового пользователя добавляем внутрь словаря users
+'''
+
+my_dict = {
+    'name': 'John',
+    'year': '1999',
+    'city': 'SP'
+}
+
+my_dict['childs'] = {
+    'name': 'Peter',
+    'year': '2001',
+}
+
+
+# import json
+# users = {}
+# p = input("пароль")
+# u_n = input("имя")
+# id = u_n + "_1"
+# users[id] = {
+#   'name': u_n,
+#   'password': p
+# }
+# print(json.dumps(users, indent=4, ensure_ascii=False))
